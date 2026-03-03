7cee40a6137cad6ecb81b6ce2fe523240e2717b5
Levi Zitting <me@levizitting.com>
2026-03-03 00:06:57 -0600

    feat: update build setup for production deploy

--- src/reveal-init.ts
+++ src/reveal-init.ts
@@ -1,5 +1,6 @@
 import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm";
+import "reveal.js/dist/reveal.css";
 import "./mint-theme.css";
 
 Reveal.initialize({
